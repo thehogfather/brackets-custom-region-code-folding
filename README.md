f310ca0f327f9d3b72e49c97c3f404fb1bad181f
Patrick Oladimeji <patrick@dustygem.co.uk>
2015-04-19 00:10:57 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# brackets-custom-region-code-folding
-Custom region code folding in brackets defined within comments
+### Custom region code folding
+
+![Alt text](./screenshot.png?raw=true "custom region")
+
+Define custom regions in any language recognised in Brackets by creating a line comment whose first word is **region** to designate the beginning of a region, and another line comment whose first word is **endregion** to designate the end of a region
+
+### License
+MIT-licensed.
---
+++ main.js
@@ -0,0 +1,89 @@
+/*
+* Copyright (c) 2013 Patrick Oladimeji. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*
+*/
+/**
+ * Custom region code folding extension for brackets
+ * @author Patrick Oladimeji
+ * @date 18/04/15 23:56:21
+ */
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, require, brackets*/
+
+require.config({
+    paths: {
+        "i18n" : "lib/i18n"
+    },
+    locale: brackets.getLocale()
+});
+
+define(function (require, exports, module) {
+    "use strict";
+    var CodeMirror              = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror"),
+        Strings                 = require("strings"),
+        AppInit                 = brackets.getModule("utils/AppInit"),
+        CommandManager          = brackets.getModule("command/CommandManager"),
+        EditorManager           = brackets.getModule("editor/EditorManager"),
+        Menus					= brackets.getModule("command/Menus"),
+        COLLAPSE_CUSTOM_REGIONS = "codefolding.collapse.customregions";
+
+    var regionFold              = require("region-fold");
+
+
+    /**
+        Collapses all custom regions defined in the current editor
+    */
+    function collapseCustomRegions() {
+        var editor = EditorManager.getFocusedEditor();
+        if (editor) {
+            var cm = editor._codeMirror, i = cm.firstLine();
+            while (i < cm.lastLine()) {
+                var range = cm.foldCode(i, {rangeFinder: regionFold});
+                if (range) {
+                    i = range.to.line;
+                } else {
+                    i++;
+                }
+            }
+        }
+    }
+
+    /**
+        Initialise the extension
+    */
+    function init() {
+        CodeMirror.registerGlobalHelper("fold", "region", function (mode, cm) {
+            return true;
+        }, regionFold);
+        /**
+         * The following is a hack to prioritise region fold over other range finders
+         */
+        var finder = CodeMirror.fold._global.pop();
+        if (finder) {
+            CodeMirror.fold._global.unshift(finder);
+        }
+
+        CommandManager.register(Strings.COLLAPSE_CUSTOM_REGIONS, COLLAPSE_CUSTOM_REGIONS, collapseCustomRegions);
+        Menus.getMenu(Menus.AppMenuBar.VIEW_MENU).addMenuItem(COLLAPSE_CUSTOM_REGIONS);
+    }
+
+    AppInit.appReady(init);
+});
